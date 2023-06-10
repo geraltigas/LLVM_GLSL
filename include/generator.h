6e1c065f545f72ba0670ed062d3bfcdc8c62b1d8
root <root@Geraltigas>
2023-06-10 16:57:14 +0800

    update: IR code gen prototype

---
+++ include/generator.h
@@ -0,0 +1,8 @@
+//
+// Created by jb030 on 04/06/2023.
+//
+
+#ifndef LLVM_GENERATOR_H
+#define LLVM_GENERATOR_H
+
+#endif //LLVM_GENERATOR_H
---
+++ include/scope.h
@@ -0,0 +1,8 @@
+//
+// Created by jb030 on 09/06/2023.
+//
+
+#ifndef LLVM_SCOPE_H
+#define LLVM_SCOPE_H
+
+#endif //LLVM_SCOPE_H
